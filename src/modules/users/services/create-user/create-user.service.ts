b9d735f15c926af968b03040d5b58657c9f0c5d9
william koller <wkoller25@gmail.com>
2021-08-29 21:27:49 -0300

    feat: add CreateUserService

---
+++ src/modules/users/services/create-user/create-user.service.ts
@@ -0,0 +1,26 @@
+import { BcryptAdapter } from '@/infra/criptography/bcript-adapter/bcrypt-adapter';
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '@/modules/users/repositories/users.repository';
+import { CreateUserDto } from '@/modules/users/dtos/create-user/create-user.dto';
+import { UserEntity } from '@/infra/db/entities/user-entity/user-entity';
+import { LoadUserByEmailService } from '@/modules/users/services/load-user-by-email/load-user-by-email.service';
+
+@Injectable()
+export class CreateUserService {
+  constructor(
+    private readonly userRepo: UsersRepository,
+    private readonly bcryptAdapter: BcryptAdapter,
+    private readonly loadUserByEmailService: LoadUserByEmailService,
+  ) {}
+
+  public async execute(createUserDto: CreateUserDto): Promise<UserEntity> {
+    await this.loadUserByEmailService.loadByEmail(createUserDto.email);
+
+    const data = {
+      ...createUserDto,
+      password: await this.bcryptAdapter.hash(createUserDto.password),
+    };
+
+    return await this.userRepo.add(data);
+  }
+}

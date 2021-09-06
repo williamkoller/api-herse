a348d35fe021394591cdac940556eebfc893577b
william koller <wkoller25@gmail.com>
2021-09-05 23:33:12 -0300

    feat: add load all users and load user by id repository

--- src/data/protocols/user/index.ts
+++ src/data/protocols/user/index.ts
@@ -1,2 +1,4 @@
 export * from './create-user.repository';
 export * from './load-user-by-email.repository';
+export * from './load-user-by-id.repository';
+export * from './load-all-users.repository';

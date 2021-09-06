0c5b2d48f00c7db26a5b4140d30cfbf36665acad
william koller <wkoller25@gmail.com>
2021-09-05 23:44:05 -0300

    refactor: updated usersController

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,13 +1,30 @@
 import { UserEntity } from '@/infra/db/entities/user-entity/user-entity';
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '@/modules/users/dtos/create-user/create-user.dto';
 import { CreateUserService } from '@/modules/users/services/create-user/create-user.service';
+import { UserIdInputDto } from '@/modules/users/dtos/user-id-input/user-id-input.dto';
+import { LoadUserByIdService } from '@/modules/users/services/load-user-by-id/load-user-by-id.service';
+import { ValidationParamsPipe } from '@/common/pipes/validation-params.pipe';
+import { UserOutputType } from '@/modules/users/types/user-output.type';
+import { LoadAllUsersService } from '@/modules/users/services/load-all-users/load-all-users.service';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly createUserService: CreateUserService) {}
+  constructor(
+    private readonly createUserService: CreateUserService,
+    private readonly loadUserByIdService: LoadUserByIdService,
+    private readonly loadAllUsersService: LoadAllUsersService,
+  ) {}
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
@@ -22,6 +39,36 @@ export class UsersController {
   public async index(
     @Body() createUserDto: CreateUserDto,
   ): Promise<UserEntity> {
-    return await this.createUserService.execute(createUserDto);
+    return await this.createUserService.add(createUserDto);
+  }
+
+  @Get('show/:id')
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Load user by id.',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User not found.',
+  })
+  public async show(
+    @Param(ValidationParamsPipe) userIdInputDto: UserIdInputDto,
+  ): Promise<UserOutputType> {
+    return await this.loadUserByIdService.loadById(userIdInputDto.id);
+  }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Load all users.',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'No record found.',
+  })
+  public async loadAll(): Promise<UserOutputType[]> {
+    return await this.loadAllUsersService.loadAll();
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,10 +6,18 @@ import { CreateUserService } from '@/modules/users/services/create-user/create-u
 import { BcryptAdapter } from '@/infra/criptography/bcript-adapter/bcrypt-adapter';
 import { LoadUserByEmailService } from '@/modules/users/services/load-user-by-email/load-user-by-email.service';
 import { UsersController } from '@/modules/users/controllers/users.controller';
+import { LoadUserByIdService } from '@/modules/users/services/load-user-by-id/load-user-by-id.service';
+import { LoadAllUsersService } from '@/modules/users/services/load-all-users/load-all-users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, UsersRepository])],
-  providers: [CreateUserService, LoadUserByEmailService, BcryptAdapter],
+  providers: [
+    CreateUserService,
+    LoadUserByEmailService,
+    LoadUserByIdService,
+    LoadAllUsersService,
+    BcryptAdapter,
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}

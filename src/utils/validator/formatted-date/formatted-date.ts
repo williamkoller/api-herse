771d5e1b1514a5bea207ab8408702ecb797ddfe9
william koller <wkoller25@gmail.com>
2021-09-17 18:55:15 -0300

    refactor: add padStart in month

--- src/utils/validator/formatted-date/formatted-date.ts
+++ src/utils/validator/formatted-date/formatted-date.ts
@@ -1,8 +1,10 @@
 export const formattedDate = (newDate: Date): string | null => {
   if (newDate) {
     const date = new Date(newDate);
-    const day = date.getDate().toString();
-    const month = (new Date(newDate).getMonth() + 1).toString();
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (new Date(newDate).getMonth() + 1)
+      .toString()
+      .padStart(2, '0');
     const year = date.getFullYear().toString();
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
